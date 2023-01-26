e61164b7ca255956ff8ef88fbe59768b8f2c35ee
Najib Belhadj <najibbelhadj11@gmail.com>
2023-01-26 21:04:23 +0100

    Client-side Date Fetching

---
+++ pages/dashboard.js
@@ -0,0 +1,33 @@
+import { useEffect, useState } from 'react'
+
+export default function Dashboard() {
+    const [isLoading, setIsLoading] = useState(true);
+    const [dashboardData, setDashboardData] = useState(null);
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const res = await fetch('http://localhost:4000/dashboard');
+                const data = await res.json();
+                setDashboardData(data);
+                setIsLoading(false);
+            } catch (error) {
+                console.log("some error", error);
+            }
+        }
+        fetchData();
+    }, [])
+    return (
+        <>
+            {
+                isLoading ? <div>Loading ..... </div> :
+                    <>
+                        <h2>Dashboard</h2>
+                        <p>Posts - {dashboardData.posts}</p>
+                        <p>Likes - {dashboardData.likes}</p>
+                        <p>Followers - {dashboardData.followers}</p>
+                        <p>Following - {dashboardData.following}</p>
+                    </>
+            }
+        </>
+    )
+}
