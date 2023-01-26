b41785fb6ae8381a192d90abaef6528c55993dbd
Najib Belhadj <najibbelhadj11@gmail.com>
2023-01-26 13:41:56 +0100

    Incremental Static Regeneration

--- pages/product/[productId]/index.js
+++ pages/product/[productId]/index.js
@@ -1,9 +1,44 @@
 import { useRouter } from "next/router"
 
-export default function ProductDetail() {
+export default function ProductDetail({ product }) {
     const router = useRouter()
-    const productId = router.query.productId
+    if (router.isFallback) {
+        return <div>Loading ....</div>
+    }
     return (
-        <h1> Details product {productId}</h1>
+        <div>
+            <h2>
+                {product.id} {product.title} {product.price}
+            </h2>
+            <p> {product.discription} </p>
+        </div>
     )
+}
+
+export async function getStaticProps(context) {
+    try {
+        const { params } = context
+        const res = await fetch(`http://localhost:4000/products/${params.productId}`)
+        const data = await res.json();
+        if (!data.id) {
+            return {
+                notFound: true
+            }
+        }
+        return {
+            props: {
+                product: data,
+            }
+        }
+    } catch (error) {
+        console.log("some error here");
+    }
+}
+
+export async function getStaticPaths() {
+
+    return {
+        paths: [{ params: { productId: '' } }],
+        fallback: true
+    }
 }
--- pages/product/index.js
+++ pages/product/index.js
@@ -1,14 +1,40 @@
 import Link from "next/link";
 
-//replace == traja3 ll home page
 
-export default function productList({ productId = 20 }) {
+
+export default function productList({ products, productId = 20 }) {
     return (
         <>
-            <Link href="/">Home</Link>
+            <h1> List of Products</h1>
+            {/* <Link href="/">Home</Link>
             <Link href="/product/"><h2>Product 1</h2></Link>
             <Link href="/product/2"><h2>Product 2</h2></Link>
-            <Link href={`/product/${productId}`} replace><h2>Product {productId} </h2></Link>
+            <Link href={`/product/${productId}`} replace><h2>Product {productId} </h2></Link> */
+            }
+            {products.map(product => {
+                return (
+                    <Link key={product.id} href={`product/${product.id}`}>
+                        <div>
+                            <h2> {product.id} {product.title} {product.price} </h2>
+                        </div>
+                    </Link>
+                )
+            })}
         </>
     )
+}
+
+
+export async function getStaticProps() {
+    try {
+        const res = await fetch('http://localhost:4000/products')
+        const data = await res.json();
+        return {
+            props: {
+                products: data,
+            }
+        }
+    } catch (error) {
+        console.log("some error here");
+    }
 }
