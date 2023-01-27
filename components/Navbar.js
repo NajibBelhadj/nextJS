9f1181f7dad8e0c552ffeec0b1da6ee1e5f154f2
Najib Belhadj <najibbelhadj11@gmail.com>
2023-01-27 02:15:40 +0100

    Sign In Sign Out

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { signIn, signOut } from 'next-auth/react'
 
 export default function Navbar() {
     return (
@@ -41,12 +42,18 @@ export default function Navbar() {
                     <Link href="/todo">ToDo</Link>
                 </li>
                 <li>
-                    <Link href='#'>
+                    <Link href='/api/auth/signin' onClick={e => {
+                        e.preventDefault()
+                        signIn(('github'))
+                    }}>
                         Sign In
                     </Link>
                 </li>
                 <li>
-                    <Link href='#'>
+                    <Link href='/api/auth/signout' onClick={e => {
+                        e.preventDefault()
+                        signOut()
+                    }}>
                         Sign Out
                     </Link>
                 </li>
