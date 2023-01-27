0a5f53a24f2627cf618eca5edf15d65a03071c7a
Najib Belhadj <najibbelhadj11@gmail.com>
2023-01-27 04:06:46 +0100

    Connecting to a database

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -5,7 +5,16 @@ export default NextAuth({
     providers: [
         GithubProviders({
             clientId: process.env.GITHUB_ID,
-            clientSecret: process.env.GITHUB_SECRET
+            clientSecret: process.env.GITHUB_SECRET,
+            checks: 'none'
         })
-    ]
+    ],
+    database: process.env.DB_URL,
+    session: {
+        strategy: "jwt"
+    },
+    jwt: {
+        secret: process.env.JWT_SECRET,
+        maxAge: 60 * 60 * 24 * 30
+    }
 })
