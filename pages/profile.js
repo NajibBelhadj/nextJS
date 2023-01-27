661c68e60a5da10e71abde0a91444ffd40b17607
Najib Belhadj <najibbelhadj11@gmail.com>
2023-01-27 03:07:57 +0100

    Securing Pages Server-side

--- pages/profile.js
+++ pages/profile.js
@@ -8,6 +8,14 @@ export default function Profile({ data }) {
 }
 export async function getServerSideProps(context) {
     const session = await getSession(context);
+    if (!session) {
+        return {
+            redirect: {
+                destination: `/api/auth/signin?callbackUrl=http://localhost:3000/profile`,
+                permanent: false
+            }
+        }
+    }
     return {
         props: {
             session,
